60ffef3862a3befb8c583031bd9e02cb7b10788d
Jon Inazio <sanchez@ingartek.com>
2015-08-19 14:34:33 +0200

    - Added another vehicle.

--- src/com/ingartek/prueba/csp/CSP/KnapsackInvertido.java
+++ src/com/ingartek/prueba/csp/CSP/KnapsackInvertido.java
@@ -8,7 +8,6 @@ import org.chocosolver.solver.objective.ObjectiveStrategy;
 import org.chocosolver.solver.objective.OptimizationPolicy;
 import org.chocosolver.solver.search.strategy.IntStrategyFactory;
 import org.chocosolver.solver.search.strategy.strategy.AbstractStrategy;
-import org.chocosolver.solver.trace.Chatterbox;
 import org.chocosolver.solver.variables.IntVar;
 import org.chocosolver.solver.variables.VariableFactory;
 
@@ -33,13 +32,14 @@ public class KnapsackInvertido extends AbstractProblem {
 	private Integer MAX_DOMAIN_VARS = 50;
 	
 	// Datos
-	private Grupo grupo = new Grupo(9);
+	private Grupo grupo = new Grupo(14);
 	private Vehiculo v1 = new Vehiculo(2, (float) 50);
 	private Vehiculo v2 = new Vehiculo(7, (float) 71);
+	private Vehiculo v3 = new Vehiculo(3, (float) 20);
 	
-	private IntVar[] variables = new IntVar[2];
-	private int[] asientosVehiculos = new int[2];
-	private int[] costesVehiculos = new int[2];
+	private IntVar[] variables = new IntVar[3];
+	private int[] asientosVehiculos = new int[3];
+	private int[] costesVehiculos = new int[3];
 	
 	private Integer sitiosServidos = 0;
 	private Integer costeTotal = 0;
@@ -47,6 +47,7 @@ public class KnapsackInvertido extends AbstractProblem {
 	// Variables
 	private IntVar varCantidadVehiculos1;
 	private IntVar varCantidadVehiculos2;
+	private IntVar varCantidadVehiculos3;
 	
 	private IntVar varCoste;
 	
@@ -64,15 +65,19 @@ public class KnapsackInvertido extends AbstractProblem {
 
 		varCantidadVehiculos1 = VariableFactory.bounded("var1", 0, MAX_DOMAIN_VARS, solver);
 		varCantidadVehiculos2 = VariableFactory.bounded("var2", 0, MAX_DOMAIN_VARS, solver);
+		varCantidadVehiculos3 = VariableFactory.bounded("var3", 0, MAX_DOMAIN_VARS, solver);
 
 		variables[0] = varCantidadVehiculos1;
 		variables[1] = varCantidadVehiculos2;
+		variables[2] = varCantidadVehiculos3;
 		
 		asientosVehiculos[0] = v1.getAsientos();
 		asientosVehiculos[1] = v2.getAsientos();
+		asientosVehiculos[2] = v3.getAsientos();
 		
 		costesVehiculos[0] = Math.round(v1.getCoste());
 		costesVehiculos[1] = Math.round(v2.getCoste());
+		costesVehiculos[2] = Math.round(v3.getCoste());
 		
 		varCoste = VariableFactory.bounded("coste", 0, 99999, solver);
 		
@@ -85,15 +90,15 @@ public class KnapsackInvertido extends AbstractProblem {
 	@Override
 	public void configureSearch() {
 		@SuppressWarnings("rawtypes")
-		AbstractStrategy strat = IntStrategyFactory.lexico_LB(new IntVar[]{varCantidadVehiculos1, varCantidadVehiculos2});
-        // trick : top-down maximization
+		AbstractStrategy strat = IntStrategyFactory.lexico_LB(new IntVar[]{varCantidadVehiculos1, varCantidadVehiculos2, varCantidadVehiculos3});
         solver.set(new ObjectiveStrategy(varCoste, OptimizationPolicy.BOTTOM_UP), strat);
-        Chatterbox.showDecisions(solver);
+        //Chatterbox.showDecisions(solver);
 	}
 
 	@Override
 	public void solve() {
-		solver.findOptimalSolution(ResolutionPolicy.MINIMIZE, varCoste);
+//		solver.findOptimalSolution(ResolutionPolicy.MINIMIZE, varCoste);
+		solver.findAllOptimalSolutions(ResolutionPolicy.MINIMIZE, varCoste, true);
 	}
 
 	@Override
@@ -101,6 +106,7 @@ public class KnapsackInvertido extends AbstractProblem {
 		StringBuilder st = new StringBuilder(String.format("Knapsack Invertido \n"));
 		st.append(String.format("\tVehículo 1. Asientos=%d - Coste=%d - Cantidad=%d\n", v1.getAsientos().intValue(), v1.getCoste().intValue(), variables[0].getValue()));
 		st.append(String.format("\tVehículo 2. Asientos=%d - Coste=%d - Cantidad=%d\n\n", v2.getAsientos().intValue(), v2.getCoste().intValue(), variables[1].getValue()));
+		st.append(String.format("\tVehículo 3. Asientos=%d - Coste=%d - Cantidad=%d\n\n", v3.getAsientos().intValue(), v3.getCoste().intValue(), variables[2].getValue()));
 		st.append(String.format("\tCoste total: %d", varCoste.getValue()));
 	}
 
